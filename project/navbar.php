f911f8c702074850ed714041cfbb4179dc51a2d6
ian <ianbitok6465@gmail.com>
2024-05-20 01:48:10 +0300

    navbar working

---
+++ project/navbar.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" integrity="sha256-2TnSHycBDAm2wpZmgdi0z81kykGPJAkiUY+Wf97RbvY=" crossorigin="anonymous">
+<nav class="navbar navbar-expand-lg navbar-light bg-dark p-5">
+  <a class="navbar-brand text-light" href="dashboard.php">Dashboard</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link text-light" href="adduser.php">Add User</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light" href="viewuser.php">View All Users</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light" href="signup.php">Sign Up</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light" href="logout.php">Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
